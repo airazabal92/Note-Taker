cfcb1f8a8a54ae65bba7716d962fe45273efad08
Amanda Irazabal <amanda.irazabal@gmail.com>
2020-05-25 13:34:41 -0400

    Fixed express app initi issue

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
-// Require express
+// Dependency
 const express = require("express");
 
-// Sets up Express App
+// Start express App
 const app = express();
-
-// Require html and api route js files
-require("./routing/api-routes.js")(app);
-require("./routing/html-routes.js")(app);
-
 const PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Allow use of static css file
 app.use(express.static("public"));
 
-// Starts server to begin listening
+require("./routing/api-routes.js")(app);
+require("./routing/html-routes.js")(app);
+
+// Initialize server
 app.listen(PORT, () => {
-  console.log("Server is listening on: http://localhost: " + PORT);
+  console.log("Server is listening on: http://localhost" + PORT);
 });
